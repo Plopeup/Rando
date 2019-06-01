dbc2e4098cba9443187fabf902621965f52879bb
Randy Maysaud <40130474+Plopeup@users.noreply.github.com>
2019-06-01 14:16:17 -0400

    Addition of WordNet

--- Main.py
+++ Main.py
@@ -1,4 +1,18 @@
+import numpy
 import pandas
-import numPy
-import stupid
-cake
+from random import randint
+from Emotional_Responses import insult, compliment, flirty, sad
+from emotion import Emotion
+from wnaffect import WNAffect
+import nltk
+
+def main():
+    wna = WNAffect('wordnet-1.6/','wn-domains-3.2/')
+    word = ['annoyance']
+    pos= nltk.pos_tag(word)
+    emo = wna.get_emotion(pos[0][0],pos[0][1])
+    print(pos)
+    print(pos[0][0])
+    print(pos[0][1])
+    print(emo)
+main()
